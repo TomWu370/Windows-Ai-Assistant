75d57712a0cf05da6a72eff4f1f4fa1c2aa1db2e
Tom Wu <tomwu370@gmail.com>
2023-12-23 15:04:54 +0000

    Finished function for mapping commands in CommandMapper.py

--- Cores/CommandMapper.py
+++ Cores/CommandMapper.py
@@ -1,24 +1,23 @@
 # Strictly Mapping Commands to Functions, with alias, no extra functionalities
-
 # commands should look like this:
 # [command, functionHeader, [aliases]]
-# functionHeader is also the filename
+# functionHeader is also the filename, use importlib to import a string name
+import importlib
+
 commandAlias = {}
 commandToFunc = {}
-def mapCommands(commands):
-    commandAlias = {}
-    commandToFunc = {}
+
+def mapCommands(commands, pathHeader = "Modules."):
     for command, func, aliases in commands:
         for alias in aliases:
             commandAlias[alias] = command
-        commandToFunc[command] = func
+        # might have issues with importing from different directories, therefore might need to use absolute path
+        commandToFunc[command] = importlib.import_module(pathHeader + func)
     return commandAlias, commandToFunc
 
 def loadPlugins(commands):
     # sometimes command might conflict with existing commands, warn user after replacing command with plugin
-    commandAlias, commandToFunc = mapCommands(commands)
-    return commandAlias, commandToFunc
-
+    return mapCommands(commands, "Plugins.")
 
 def readCommands():
     for command in commandToFunc.keys():
