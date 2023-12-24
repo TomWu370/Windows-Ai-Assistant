7b13502e59d37353ce66beed368dd6b71d90a1ca
Tom Wu <tomwu370@gmail.com>
2023-12-24 01:11:28 +0000

    Implemented template code for Text2Speech.py, including pause and resume audio test Added template code for Notification.py Core

---
+++ Cores/Notification.py
@@ -0,0 +1,22 @@
+
+
+
+
+class Notification:
+
+
+
+
+
+
+    def Notify(self, message):
+        print(message)
+        return None
+
+    def Warn(self, message):
+        print(message)
+        return None
+
+    def Alert(self, message):
+        print(message)
+        return None
--- Cores/Text2Speech.py
+++ Cores/Text2Speech.py
@@ -1,2 +1,15 @@
-# for 1 option, this module will use local wav files and convert using RCV voice change models for tts speak
-# otherwise, normal tts
+import time
+
+from RealtimeTTS import TextToAudioStream, SystemEngine, AzureEngine, ElevenlabsEngine
+
+engine = SystemEngine() # replace with your TTS engine
+stream = TextToAudioStream(engine)
+print("hi")
+stream.feed("Hello world! How are you today?")
+stream.play_async()
+while stream.is_playing():
+    time.sleep(0.4)
+    stream.pause()
+    time.sleep(0.4)
+    stream.resume()
+
