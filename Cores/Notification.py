cb02bbeed1181344bed966e913674a6e1518bf4a
Tom Wu <tomwu370@gmail.com>
2023-12-25 23:59:15 +0000

    Added logic for notification interruption Added voice changing mechanic for RealTimeTTS

--- Cores/Notification.py
+++ Cores/Notification.py
@@ -1,15 +1,24 @@
+import threading
+
+from RealtimeTTS import SystemEngine, TextToAudioStream
+
+from Cores.Text2Speech import TextToSpeech
 
 
+class Notification(TextToSpeech):
 
-
-class Notification:
-
-    def __init__(self, tts):
+    def __init__(self, tts, voice):
+        # initialise TextToSpeech class, to get engine object
+        super().__init__(voice)
         self.tts = tts
 
-
     def Notify(self, message):
-        print(message)
+        # interrupts any currently playing tts, then play notification until completion, then resume tts
+        self.tts.pause()
+        notif_thread = threading.Thread(target=self._say, args=(message,))
+        notif_thread.start()
+        notif_thread.join()
+        self.tts.resume()
         return None
 
     def Warn(self, message):
@@ -25,4 +34,3 @@ class Notification:
 
     def resume(self):
         self.tts.resume()
-
--- Cores/Text2Speech.py
+++ Cores/Text2Speech.py
@@ -5,11 +5,14 @@ from RealtimeTTS import TextToAudioStream, SystemEngine, AzureEngine, Elevenlabs
 
 
 class TextToSpeech:
-    def __init__(self):
-        system = SystemEngine()  # replace with your TTS engine
+    def __init__(self, voice):
+        self.voice = voice
+        system = SystemEngine(voice=voice, print_installed_voices=True)  # replace with your TTS engine
         self.engine = TextToAudioStream(system)
 
     def _say(self, text):
+        print(self.voice)
+        self.engine.engine.set_voice(self.voice)
         self.engine.feed(text)
         self.engine.play()
         # while self.engine.is_playing(): will only work with Notification interruption when using play_async
@@ -17,28 +20,29 @@ class TextToSpeech:
 
 
     def say(self, text):
-        my_thread = threading.Thread(target=self._say, args=(text,))
-        my_thread.start()
-        # my_thread.join() # check if this is needed
+        threading.Thread(target=self._say, args=(text,)).start()
+        # my_thread.join() # not needed because it locks until completion, needed for Notification interruption
 
     def pause(self):
         self.engine.pause()
 
     def resume(self):
         self.engine.resume()
+    def test(self):
+        print("at test")
 
     class b:
         pass
         # activate pause
 
 
-tts = TextToSpeech()
-print("hi")
-#tts.say("Hello world! How are you today? Hello world! How are you today? Hello world! How are you today? Hello world! How are you today?")
-print(tts.engine.is_playing())
-time.sleep(3)
-tts.pause()
-print("hi here")
-time.sleep(10)
-tts.resume()
-print("hi final")
+# tts = TextToSpeech("a")
+# print("hi")
+# tts.say("Hello world! How are you today?")
+# print(tts.engine.is_playing())
+# time.sleep(3)
+# tts.pause()
+# print("hi here")
+# time.sleep(5)
+# tts.resume()
+# print("hi final")
